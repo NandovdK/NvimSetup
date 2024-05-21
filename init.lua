51508ffd651e938c83a18deb88d3df1949b96ac1
NandovdK <njvdkant@gmail.com>
2024-05-21 13:29:49 +0200

    add keybind to toggle lsp diagnostics

--- init.lua
+++ init.lua
@@ -119,6 +119,13 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>d",
+	[[:lua if vim.diagnostic.is_disabled() then vim.diagnostic.enable() else vim.diagnostic.disable() end<CR>]],
+	{ noremap = true, silent = true }
+)
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
